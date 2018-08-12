ac2d92358b3ae042725d067dbe46d7ecc0e14a5c
Jayon Kim <jpkim921@gmail.com>
2018-08-12 18:16:56 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,21 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    # returns the first five Boats
+    # boats = Boat.all.map {|boat| boat.name if boat.id <= 5}.compact
+    # binding.pry
+    Boat.all.limit(5)
+  end
+
+  def self.dinghy
+    # Boat.all.map {|boat| boat.name if boat.length < 20}.compact
+    Boat.all.where(length < 20)
+  end
+
+  def self.ship
+    Boat.all.select {|boat| boat.name if boat.length >= 20}.compact
+  end
+
 end
